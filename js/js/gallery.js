169005a68cbc2f76f933ba868586c1c5f5b61390
Semih <semihkundakci5@gmail.com>
2023-02-17 12:47:06 +0300

    JsCourse

---
+++ js/js/gallery.js
@@ -0,0 +1,21 @@
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+  
+	}
+
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+		
+	}
