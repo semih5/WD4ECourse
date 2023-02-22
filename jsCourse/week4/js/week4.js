2e1ac676ae1a8ca1fe8f1a387f22bf4ce6203ea6
Semih <semihkundakci5@gmail.com>
2023-02-22 12:59:43 +0300

    update

--- jsCourse/week4/js/week4.js
+++ jsCourse/week4/js/week4.js
@@ -1,9 +1,9 @@
 function billingFunction() {
 	if (document.getElementById("same").checked) {
 		document.getElementById("billingName").value= document.getElementById("shippingName");
-		document.getElementById("billingZip").value= document.getElementById("shippingZip")
+		document.getElementById("billingZip").value= document.getElementById("shippingZip");
 	}else{
 		document.getElementById("billingName").value="";
-		document.getElementById("billingZip").value=""
+		document.getElementById("billingZip").value="";
 	}
 }
